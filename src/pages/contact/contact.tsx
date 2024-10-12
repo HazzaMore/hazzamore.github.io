fa15e957bf3cbf617c80324c3f0853003780dc89
HazzaMore <mrhazar.moore@gmail.com>
2024-10-12 12:59:54 +0100

    Created template page under constructions and error page

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -1,9 +1,50 @@
+import "../../App.css"
+import { useContext } from "react";
+import { MenuContext } from "../../App";
+import styled from "styled-components";
 
 
 const Contact = () => {
+
+  const { menuActive} = useContext(MenuContext);
+
   return (
-    <div>contact me</div>
+    
+    <ContactWrapper menuActive={menuActive}>
+      <div className="background TransformRight"/>
+      <section className="text TransformRight" data-aos="fade">
+        <h1>
+          Contact Me
+          <br/>
+          Sorry this page is under construction
+        </h1>
+      </section>
+
+    </ContactWrapper>
   )
 }
 
 export default Contact;
+
+  const ContactWrapper = styled.div<{ menuActive: boolean}>`
+
+  .TransformRight {
+    transform: translateX(${ props => props.menuActive ? `300px` : '0'});
+    transition: 0.3s ease;
+  }
+
+  h1 {
+    font-size: 3em;
+    font-weight: 700;
+    color: #fff;
+    line-height: 1em;
+    text-transform: uppercase;
+  }
+
+  .text {
+    vertical-align: middle;
+    z-index: 10;
+    padding-top: 40vh;
+    padding-left: 10%;
+  }
+`;
