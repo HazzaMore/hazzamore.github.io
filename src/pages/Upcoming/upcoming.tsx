b4b077a6057d75c8aadcf18f462359629c971027
HazzaMore <mrhazar.moore@gmail.com>
2024-10-18 14:29:20 +0100

    update upcoming list

--- src/pages/Upcoming/upcoming.tsx
+++ src/pages/Upcoming/upcoming.tsx
@@ -6,6 +6,7 @@ export const upcoming = () => {
       This will feature a check list of all the upcoming features to the site
       - Add the new upcoming page
       - Animations
+      - To top button required for about me page
       - Improve Portfolio card designs
       - Add loading Animations
       - Add a login step so owner can update the site
