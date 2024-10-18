f398c49e8fdb760073d0c8871cd01a2a59b04d8c
HazzaMore <mrhazar.moore@gmail.com>
2024-10-18 15:12:40 +0100

    css styles added to upcoming

--- src/pages/Upcoming/upcoming.tsx
+++ src/pages/Upcoming/upcoming.tsx
@@ -12,6 +12,7 @@ export const upcoming = () => {
       - Add a login step so owner can update the site
       - Connect the site to a database to allow for dynamic content
       - Improve the CV page design
+      - Improve css to have additional classNames for repeated styles (e.g. left margin)
     </div>
   )
 }
