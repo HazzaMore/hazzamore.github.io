0c3637a82aa6a2498ac4d92e9434089497185a54
HazzaMore <mrhazar.moore@gmail.com>
2024-10-18 14:28:37 +0100

    Initial upcoming section added as a reminder to add it

---
+++ src/pages/Upcoming/upcoming.tsx
@@ -0,0 +1,16 @@
+
+
+export const upcoming = () => {
+  return (
+    <div>
+      This will feature a check list of all the upcoming features to the site
+      - Add the new upcoming page
+      - Animations
+      - Improve Portfolio card designs
+      - Add loading Animations
+      - Add a login step so owner can update the site
+      - Connect the site to a database to allow for dynamic content
+      - Improve the CV page design
+    </div>
+  )
+}
