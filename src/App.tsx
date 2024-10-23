629326cb0bf2a315565cebb725b2e80b4a190509
HazzaMore <mrhazar.moore@gmail.com>
2024-10-23 16:52:23 +0100

    Changes to bring single page functionality to github pages

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="Showcase">
       <MenuContext.Provider value={{menuactive, handleToggle}}>
-        <Router>
+        <Router basename="/repo-name">
         <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />}/>
